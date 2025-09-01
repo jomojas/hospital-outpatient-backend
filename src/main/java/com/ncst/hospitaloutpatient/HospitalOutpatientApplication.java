fe03c0069032fd0521e4b8d22a16e0b0bcfb0a23
Jomo <jomojassen@gmail.com>
2025-09-01 10:31:36 +0800

    First Commit

---
+++ src/main/java/com/ncst/hospitaloutpatient/HospitalOutpatientApplication.java
@@ -0,0 +1,13 @@
+package com.ncst.hospitaloutpatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalOutpatientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalOutpatientApplication.class, args);
+    }
+
+}
